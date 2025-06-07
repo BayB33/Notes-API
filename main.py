0e6d1b20f7dd251001caa5a1ca478dd9342915a0
BayB33 <thomasraymond.d@gmail.com>
2025-06-07 13:01:49 -0400

    auto commit

--- main.py
+++ main.py
@@ -49,3 +49,5 @@ def update_note(note_id: int, updated_note: Note):
             notes[index] = updated_note
             return {"message": "Note modifiee avec succes"}
     raise HTTPException(status_code=404, detail="Note non trouvee")
+
+#test auto push 4
